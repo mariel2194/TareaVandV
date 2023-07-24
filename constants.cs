611900d9851fdaa0372fec4643cf50f61f9651fb
mariel2194 <21648737-mariel2194@users.noreply.replit.com>
2023-07-24 16:28:11 +0000

    Exceptions

---
+++ constants.cs
@@ -0,0 +1,25 @@
+public class IsrConstants
+{ 
+ public IsrConstants(){}
+  
+ public struct escala
+ {
+    public const double min = 416220.01;
+    public const double med = 624329.01;
+    public const double alt = 867123.00;
+ }
+ 
+ public struct porcentaje
+ {
+    public const double min = 0.15;
+    public const double med = 0.20;
+    public const double alt = 0.25;
+ }
+
+public struct sumando
+ {
+    public const double min = 0;
+    public const double med = 31216.00;
+    public const double alt = 79776.00;
+ }
+}
--- main.cs
+++ main.cs
@@ -2,27 +2,27 @@ using System;
 
 public class CalculeISR
 {
-  
-public struct escala
-{
-    public const double min = 416220.01;
-    public const double med = 624329.01;
-    public const double alt = 867123.00;
-}
 
-public struct porcentaje
-{
-    public const double min = 0.15;
-    public const double med = 0.20;
-    public const double alt = 0.25;
-}
+    public struct escala
+    {
+        public const double min = 416220.01;
+        public const double med = 624329.01;
+        public const double alt = 867123.00;
+    }
 
-public struct sumando
-{
-    public const double min = 0;
-    public const double med = 31216.00;
-    public const double alt = 79776.00;
-}
+    public struct porcentaje
+    {
+        public const double min = 0.15;
+        public const double med = 0.20;
+        public const double alt = 0.25;
+    }
+
+    public struct sumando
+    {
+        public const double min = 0;
+        public const double med = 31216.00;
+        public const double alt = 79776.00;
+    }
 
     public static void Main(string[] args)
     {
@@ -30,41 +30,65 @@ public struct sumando
         double sueldoAnual = 0;
         double IsrTotal = 0;
         double excedente = 0;
-
-        Console.WriteLine("Introduce tu sueldo anual");
-        double sueldo = Double.Parse(Console.ReadLine());
-        sueldoAnual = sueldo * 12;
-
-        if (sueldoAnual <= escala.min)
-        {
-            IsrTotal = 0;
-            Console.WriteLine("El ISR a pagar es: " + IsrTotal);
-        }
-        else if (sueldoAnual > escala.min && sueldoAnual <= escala.med)
-        {
-            excedente = sueldoAnual - escala.min;
-            IsrTotal = ((excedente * porcentaje.min) + sumando.min) / 12;
-            Console.WriteLine("El ISR a pagar es: " + IsrTotal);
-        }
-        else if (sueldoAnual > escala.med && sueldoAnual <= escala.alt)
-        {
-            excedente = sueldoAnual - escala.med;
-            IsrTotal = ((excedente * porcentaje.med) + sumando.med) / 12;
-            Console.WriteLine("El ISR a pagar es: " + IsrTotal);
-
-        }
-        else if (sueldoAnual > escala.alt)
-        {
-            excedente = sueldoAnual - escala.alt;
-            IsrTotal = ((excedente * porcentaje.alt) + sumando.alt) / 12;
-            Console.WriteLine("El ISR a pagar es: " + IsrTotal);
-        }
-        else
-        {
-            Console.WriteLine("Monto Invalido, Digite nuevamente");
-        }
       
-       Environment.Exit(0);
+        
+        Console.WriteLine("Introduce tu sueldo anual");
+        try
+        {
+            double sueldo = Double.Parse(Console.ReadLine());
+            if (sueldo < 0)
+            {
+
+                {
+                    Console.WriteLine("Monto Invalido");
+                }
+
+            }
+            else
+            {
+                sueldoAnual = sueldo * 12;
+
+                if (sueldoAnual <= escala.min)
+                {
+                    IsrTotal = 0;
+                    Console.WriteLine("El ISR a pagar es: " + IsrTotal);
+                }
+                else if (sueldoAnual > escala.min && sueldoAnual <= escala.med)
+                {
+                    excedente = sueldoAnual - escala.min;
+                    IsrTotal = ((excedente * porcentaje.min) + sumando.min) / 12;
+                    Console.WriteLine("El ISR a pagar es: " + IsrTotal);
+                }
+                else if (sueldoAnual > escala.med && sueldoAnual <= escala.alt)
+                {
+                    excedente = sueldoAnual - escala.med;
+                    IsrTotal = ((excedente * porcentaje.med) + sumando.med) / 12;
+                    Console.WriteLine("El ISR a pagar es: " + IsrTotal);
+
+                }
+                else if (sueldoAnual > escala.alt)
+                {
+                    excedente = sueldoAnual - escala.alt;
+                    IsrTotal = ((excedente * porcentaje.alt) + sumando.alt) / 12;
+                    Console.WriteLine("El ISR a pagar es: " + IsrTotal);
+                }
+
+
+                Environment.Exit(0);
+            }
+        }
+        catch (System.FormatException)
+        {
+            Console.WriteLine("Entrada Invalida, Favor ingrese una cantidades numericas");
+        }
+        finally
+        {
+            Console.WriteLine("Intentelo Nuevamente");
+        }
+
+        Environment.Exit(0);
+
+
     }
 }
 
